f8ffa79ffc98e01aee20e553579069330acca4f5
Robin Abrahamsson <robin@abrahamsson.tech>
2022-01-06 22:21:21 +0100

    first commit

---
+++ pack.pl
@@ -0,0 +1,6 @@
+name(sudoku-utils).
+title('Example package with utils for working with Sudoku').
+version('0.0.1').
+
+home('https://github.com/rq-abrahamsson/sudoku-utils').
+download('https://github.com/rq-abrahamsson/sudoku-utils/releases/*.zip').
