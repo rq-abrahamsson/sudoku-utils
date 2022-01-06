fa5db9f2b0e05d83f9bf115f82590ffde6a18c29
Robin Abrahamsson <robin@abrahamsson.tech>
2022-01-06 22:50:49 +0100

    Fixed versioning..

--- pack.pl
+++ pack.pl
@@ -1,6 +1,6 @@
 name(sudoku_utils).
 title('Example package with utils for working with Sudoku').
-version('0.0.5').
+version('0.0.6').
 
 home('https://github.com/rq-abrahamsson/sudoku_utils').
 download('https://github.com/rq-abrahamsson/sudoku_utils/releases/*.zip').
