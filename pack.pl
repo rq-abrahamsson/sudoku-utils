5b5d725aa914ee7a985a64dfd28f8e0fc11c5085
Robin Abrahamsson <robin@abrahamsson.tech>
2022-01-06 22:43:45 +0100

    Test bumping version correctly..

--- pack.pl
+++ pack.pl
@@ -1,6 +1,6 @@
 name(sudoku-utils).
 title('Example package with utils for working with Sudoku').
-version('0.0.1').
+version('0.0.4').
 
 home('https://github.com/rq-abrahamsson/sudoku-utils').
 download('https://github.com/rq-abrahamsson/sudoku-utils/releases/*.zip').
